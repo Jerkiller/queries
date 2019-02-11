1a361f02dcc8bd1b31c1d920f90fd35a088c4faf
Jerkiller <luca.favorido@gmail.com>
2019-02-11 14:35:53 +0100

    Update TSQL.sql

--- TSQL.sql
+++ TSQL.sql
@@ -40,3 +40,39 @@ CONCAT(
 FROM sys.master_files WITH(NOWAIT)
 WHERE database_id = DB_ID() -- for current db 
 GROUP BY database_id
+
+							    
+							    
+### Last Slow Queries
+							    
+SELECT TOP 100 SUBSTRING(qt.TEXT, (qs.statement_start_offset/2)+1,
+((CASE qs.statement_end_offset
+WHEN -1 THEN DATALENGTH(qt.TEXT)
+ELSE qs.statement_end_offset
+END - qs.statement_start_offset)/2)+1),
+qs.execution_count,
+qs.total_logical_reads, qs.last_logical_reads,
+qs.total_logical_writes, qs.last_logical_writes,
+qs.total_worker_time,
+qs.last_worker_time,
+qs.total_elapsed_time/1000000 total_elapsed_time_in_S,
+qs.last_elapsed_time/1000000 last_elapsed_time_in_S,
+qs.last_execution_time,
+qp.query_plan
+FROM sys.dm_exec_query_stats qs
+CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
+CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+-- ORDER BY qs.total_logical_reads DESC -- logical reads
+-- ORDER BY qs.total_logical_writes DESC -- logical writes
+ ORDER BY qs.total_worker_time DESC -- CPU time
+
+												
+												
+### Table names										
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES
+
+### Table column	
+ SELECT * 
+  FROM INFORMATION_SCHEMA.COLUMNS 
+  WHERE TABLE_NAME = 'myTableName'
